6d69311794d3dd6a4f506c6dc1fc3860436120c1
Josh Frank <aqualis@gmail.com>
2021-09-28 23:54:33 -0500

    add sums of squares of digits of numbers in list

--- src/Racket/rktscratch0001.rkt
+++ src/Racket/rktscratch0001.rkt
@@ -1,3 +1,14 @@
 #lang racket
 
-(+ 3 4)
+(define sample (list 10 21 32 43 54 65))
+
+(define (square x) (* x x))
+
+(define (addSquareDigits x)
+	(if (> x 0)
+		(+ (square (remainder x 10)) (addSquareDigits (floor (/ x 10))))
+		0
+	)
+)
+
+(map addSquareDigits sample)
